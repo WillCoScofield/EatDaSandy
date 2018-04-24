5f427df2df347924c37ac9d51c2142781e3098eb
Connor <wscofield@elon.edu>
2018-04-24 19:29:09 -0400

    initial file structure, DB creation and server connection complete

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE food_db;
+USE food_db;
+
+CREATE TABLE sandys(
+    id INT NOT NULL AUTO_INCREMENT,
+    sandy_name VARCHAR (100) NOT NULL, 
+    consumed BOOLEAN default false
+)
+
---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO sandys (sandy_name) VALUES ("Philly Cheese Steak"), ("BLT"), ("Grilled Cheese"); 
